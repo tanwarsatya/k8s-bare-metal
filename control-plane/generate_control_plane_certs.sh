18e4f724868d1aabeac03e9e161f234c450b9641
tanwarsatya <satya.tanwar@gmail.com>
2021-03-13 07:11:05 -0500

    updated the control-plane k8s install for rsync and etcd files

--- control-plane/generate_control_plane_certs.sh
+++ control-plane/generate_control_plane_certs.sh
@@ -4,7 +4,7 @@ echo "--------------------------------"
 echo "control plane - generate certs"
 echo "--------------------------------"
 echo "1. Generating admin client cert"
-echo "--------------------------------"
+
 
 # pre steps
 # create output directory
@@ -35,32 +35,31 @@ cfssl gencert \
   -profile=default \
    config/admin-csr.json | cfssljson -bare output/admin
 
-echo "**********************************"
+
 echo "2. Generating kube-controller-manager cert"
-echo "--------------------------------"
+
 cfssl gencert \
   -ca=../cert-authority/certs/ca.pem \
   -ca-key=../cert-authority/certs/ca-key.pem \
   -config=../cert-authority/config/ca-config.json \
   -profile=default \
   config/kube-controller-manager-csr.json | cfssljson -bare output/kube-controller-manager
-echo "**********************************"
 
-echo "**********************************"
-echo "3. Generating kube-scheduler cert"
-echo "--------------------------------"
+
+echo "3. Generating kube-scheduler cert "
+
 cfssl gencert \
   -ca=../cert-authority/certs/ca.pem \
   -ca-key=../cert-authority/certs/ca-key.pem \
   -config=../cert-authority/config/ca-config.json \
   -profile=default \
   config/kube-scheduler-csr.json | cfssljson -bare output/kube-scheduler
-echo "**********************************"
 
 
-echo "**********************************"
+
+
 echo "4. Generating kube-apiserver cert"
-echo "--------------------------------"
+
 
 # Set host names to be used
 KUBERNETES_HOSTNAMES=kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local
@@ -72,12 +71,12 @@ cfssl gencert \
   -hostname=10.32.0.1,127.0.0.1,${KUBERNETES_HOSTNAMES},${CONTROL_PLANE_NODE_IPS} \
   -profile=default \
   config/kube-apiserver-csr.json | cfssljson -bare output/kub-apiserver
-echo "**********************************"
 
 
-echo "**********************************"
+
+
 echo "4. Generating etcd cert"
-echo "--------------------------------"
+
 
 # Set host names to be used
 KUBERNETES_HOSTNAMES=kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local
@@ -89,16 +88,15 @@ cfssl gencert \
   -hostname=10.32.0.1,127.0.0.1,${KUBERNETES_HOSTNAMES},${CONTROL_PLANE_NODE_IPS} \
   -profile=default \
   config/etcd-csr.json | cfssljson -bare output/etcd
-echo "**********************************"
 
 
-echo "**********************************"
-echo "5. Generating service-account cert"
-echo "--------------------------------"
+
+
+echo "5. Generating service-account cert -----------------------------"
+
 cfssl gencert \
   -ca=../cert-authority/certs/ca.pem \
   -ca-key=../cert-authority/certs/ca-key.pem \
   -config=../cert-authority/config/ca-config.json \
   -profile=default \
   config/service-account-csr.json | cfssljson -bare output/service-account
-echo "**********************************"
--- control-plane/install_control_plane_k8s.sh
+++ control-plane/install_control_plane_k8s.sh
@@ -6,36 +6,15 @@ echo "control plane k8s - installation"
 
 # set default variables
 SSH_USER="stanwar"
-SSH_CERT=""
+SSH_CERT="/home/stanwar/.ssh/id_rsa"
 
 
-echo "--------------------------------"
-echo "0. disable control plane services  "
-echo "--------------------------------"
-
-echo "Stop and disable etcd..."
+echo "Stop and disable installed services"
 # stop and disable etcd services if running
-sudo systemctl stop etcd kube-apiserver kube-scheduler kube-controller-manager
-sudo systemctl disable etcd kube-apiserver kube-scheduler kube-controller-manager
+#sudo systemctl stop etcd kube-apiserver kube-scheduler kube-controller-manager
+#sudo systemctl disable etcd kube-apiserver kube-scheduler kube-controller-manager
 
-echo "Stop and disable kube-apiserver..."
-# stop and disable etcd services if running
-sudo systemctl stop kube-apiserver
-sudo systemctl disable kube-apiserver
-
-echo "Stop and disable kube-scheduler..."
-# stop and disable etcd services if running
-sudo systemctl stop kube-scheduler
-sudo systemctl disable kube-scheduler
-
-echo "Stop and disable kube-controller-manager..."
-# stop and disable etcd services if running
-sudo systemctl stop kube-controller-manager
-sudo systemctl disable kube-controller-manager
-
-echo "--------------------------------"
-echo "1. Generate root cert "
-echo "--------------------------------"
+#generate root cert if not available 
 CA_PEM_FILE=../cert-authority/certs/ca.pem
 CA_KEY_FILE=../cert-authority/certs/ca-key.pem
 
@@ -47,23 +26,19 @@ sudo mkdir -p ../cert-authority/certs
 cfssl gencert -initca ../cert-authority/config/ca-csr.json | cfssljson -bare ../cert-authority/certs/ca
 fi
 
-echo "--------------------------------"
-echo "2. Generate Control Plane certs "
-echo "--------------------------------"
+# Generate control plane certs
 sudo bash generate_control_plane_certs.sh
 
-echo "--------------------------------"
-echo "3. Generate Control Plane configs "
-echo "--------------------------------"
+# Generate conro
 sudo bash generate_control_plane_configs.sh
 
 
 echo "--------------------------------"
-echo "2. Downlaod binaries "
+echo "Downlaod binaries "
 echo "--------------------------------"
 
-# echo "Download and extract etcd binaries - etcd v3.4.10"
-# mkdir -p binaries
+#echo "Download and extract etcd binaries - etcd v3.4.10"
+#mkdir -p binaries
 
 # wget -q --show-progress --https-only --timestamping -P binaries \
 #   "https://github.com/etcd-io/etcd/releases/download/v3.4.10/etcd-v3.4.10-linux-amd64.tar.gz"
@@ -71,7 +46,7 @@ echo "--------------------------------"
 # sudo tar -C binaries -xvf binaries/etcd-v3.4.10-linux-amd64.tar.gz
 
 echo "--------------------------------"
-echo "2. Install ETCD service remotely on all master nodes"
+echo "Install ETCD service remotely on all master nodes"
 echo "--------------------------------"
 
 mapfile -t NODE_HOSTNAMES < control-plane-nodes.txt
@@ -87,41 +62,36 @@ do
     echo "________________________________________________________"
     
     echo "sync the k8s_bare_metal folder to the node"
-    sudo rsync -avz  -e "ssh -v -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa" k8s-bare-metal $SSH_USER@$NODE_NAME:/home/$SSH_USER
+    sudo rsync -avz  -e "ssh -o StrictHostKeyChecking=no -i $SSH_CERT" ../../k8s-bare-metal $SSH_USER@$NODE_NAME:/home/$SSH_USER
 
-    # echo "Creating etcd directories"
-    # ssh -i /home/$SSH_USER/.ssh/id_rsa -o StrictHostKeyChecking=no $SSH_USER@$NODE_NAME /bin/bash << EOF 
+    echo "executing remote shell commands"
     
-    # #echo create k8s_bare_metal directory
+    ssh -i $SSH_CERT -o StrictHostKeyChecking=no $SSH_USER@$NODE_NAME /bin/bash << EOF 
+    
+   
 
-    # echo "download the  hub
+    # echo "download the
     # sudo mkdir -p /home/$SSH_USER/k8s_bare_metal
     # sudo chmod -R 777 /home/$SSH_USER/k8s_bare_metal; 
     # sudo mkdir -p /home/$SSH_USER/k8s_bare_metal
 
 
-    # # Disale existing services
-    # echo "stop and disable etcd service";
-    # sudo systemctl stop etcd 
-    # sudo systemctl disable etcd 
+    # Disale existing services
+     echo "stop and disable etcd service";
+     sudo systemctl stop etcd 
+     sudo systemctl disable etcd 
     
-    # # Create directories for etcd
+    # Create directories for etcd
+      echo "Creating etcd directories"
+      sudo mkdir -p /etc/etcd /var/lib/etcd;
+      sudo chmod 700 /etc/etcd; 
+      sudo chmod 700 /var/lib/etcd; 
+
+    # copy etcd,etcdctl to /usr/local/bin
+      sudo cp /home/$SSH_USER/k8s-bare-metal/control-plane/binaries/etcd-v3.4.10-linux-amd64/etcd* /usr/local/bin 
+
     
-    # echo "Creating etcd directories"
-    # sudo mkdir -p /etc/etcd /var/lib/etcd;
-    # sudo chmod 700 /etc/etcd; echo "--------------------------------"
+EOF
 
-
-    # sudo chmod 700 /var/lib/etcd; 
-
-
-    
-    #EOF
-
-    # echo "Copy certs"
-    # scp -i /home/$SSH_USER/.ssh/id_rsa ../cert-authority/certs/ca.pem output/etcd.pem output/etcd-key.pem $SSH_USER@$NODE_NAME:/home/$SSH_USER/k8s_bare_metal
-
-    #echo "Copy etcd.service file"
-   # scp -i ~/.ssh/id_rsa output/etcd.service ${SSH_USER}@${NODE_NAME}:/etc/systemd/system
 
 done
