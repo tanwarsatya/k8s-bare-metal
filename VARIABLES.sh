a52cad5fccbd2900150b6bc5ddc0ca62cdf3b4ce
tanwarsatya <satya.tanwar@gmail.com>
2021-03-13 20:53:54 -0500

    added variables file

--- VARIABLES.sh
+++ VARIABLES.sh
@@ -1,4 +1,8 @@
 # control plane variables
 # -----------------------------------------------------------
 CONTROL_PLANE_NODES = (k8s-master-1 k8s-master-2 k8s-master-3)
+CONTROL_PLANE_API_LOAD_BALANCER_NODE = "k8s-master-lb"
+CONTROL_PLANE_SSH_USER = "stanwar"
+CONTROL_PLANE_SSH_CERT = "/home/stanwar/.ssh/id_rsa"
 # -----------------------------------------------------------
+
