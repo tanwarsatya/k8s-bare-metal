055b9a97168cf9bbcbd2b1cc5191c1f10527391e
Satya Tanwar <satya.tanwar@gmail.com>
2021-11-15 16:42:11 -0500

    moved network variable up

--- variables.sh
+++ variables.sh
@@ -17,6 +17,15 @@ declare -a CONTROL_PLANE_ETCD_NODES=("k8s-master-1")
 declare -a WORKER_PLANE_NODES=("k8s-node-1" "k8s-node-2")
 CLUSTER_API_LOAD_BALANCER="k8s-master-1"
 
+#Network Range and CNI
+#############################################
+# Cluster cidr
+CLUSTER_CIDR="10.32.0.0/12"
+# Cluster Service CIDR
+CLUSTER_SVC_CIDR="10.32.0.0/16"
+# choose a provide from list [kube-router , calico , cilium ]
+CLUSTER_CNI_PROVIDER="cilium"
+
 # Version 
 ##############################################
 # CFSSL Version
@@ -34,13 +43,6 @@ WORKER_PLANE_CNI_PLUGIN_VERSION="v1.0.1"
 # CRI Tools Version
 WORKER_PLANE_CRI_TOOLS_VERSION="v1.22.0"
 
-# Network Range and config choices
-# Cluster cidr
-CLUSTER_CIDR="10.32.0.0/12"
-# Cluster Service CIDR
-CLUSTER_SVC_CIDR="10.32.0.0/16"
-# choose a provide from kube-router , calico , cilium 
-CLUSTER_CNI_PROVIDER="cilium"
 # max number of pods to run on a node -default is 110
 WORKER_PLANE_MAX_PODS=200
 # Cluster TLS BOOT STRAPPING ALLOWED FOR WORKER NODES ( NOT TESTED YET - keep it false for now)
